c6d2e20737385b5d59599d513b76757edd0c40cf
Jared Fuller <jaredshane@gmail.com>
2016-12-01 10:11:00 -0600

    created js folder and separated the 4 assignments into 3 js files, linked scripts to HTML and corrected some console logging

---
+++ js/ants.js
@@ -0,0 +1,17 @@
+var antSpecies = {
+  argentine: {},
+  army: {},
+  bigHeaded: {},
+  black: {},
+  bull: {},
+  carpenter: {},
+  crazy: {},
+  fire: {},
+  glider: {},
+  honeyPot: {},
+  jackJumper: {}
+}
+
+for (var i in antSpecies) {
+	console.log(i)
+}
---
+++ js/math.js
@@ -0,0 +1,20 @@
+/* Write a for loop that uses a counter variable
+initialized at 5 and then increments it by 10 every
+ time it executes. Use console.log() to output the
+ value of the counter variable each time through the
+ loop. Stop execution of the loop if the counter
+ variable's value is greater than 120. */
+
+for (var i = 5; i <= 120; i += 10) {
+	console.log("Current value is " + i)
+}
+
+/* Write a for loop with a counter variable initialized
+at 4096. Each time the loop executes divide the counter
+variable's value by 2. Use console.log() to output
+its value every time. When the counter variable's value
+is 1, stop execution. */
+
+for (var i = 4096; i >= 1; i /= 2) {
+	console.log("Current value is " + i)
+}
